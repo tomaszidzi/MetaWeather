afdce06d388151df0d00de576869dbd0cb0501aa
Tomasz Idzi <idzitomek@gmail.com>
2021-03-03 23:01:40 +0100

    Handle MetaWeather API.

--- MetaWeather/MetaWeather/ForecastContainer/ForecastContainerViewController.swift
+++ MetaWeather/MetaWeather/ForecastContainer/ForecastContainerViewController.swift
@@ -14,18 +14,18 @@ class ForecastContainerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let city = CityModel(name: "Gothenburg", code: "890869")
+        
+        WeatherManager(session: URLSession.shared).currentWeather(forCity: city) { result in
+            DispatchQueue.main.async {
+                // TODO: Handle response
+//                switch result {
+//                case .success(let weather):
+//                    // TODO: Display weather on UI
+//                case .failure(let error):
+//                    // TODO: Display error message
+//                }
+            }
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
