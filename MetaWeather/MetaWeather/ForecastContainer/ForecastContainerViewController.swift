1690baa3272e18ded0b61816579367a52e52451c
Tomasz Idzi <idzitomek@gmail.com>
2021-03-03 22:09:18 +0100

    Removed Main.storyboard and setup ForecastContainerVC as init VC.

---
+++ MetaWeather/MetaWeather/ForecastContainer/ForecastContainerViewController.swift
@@ -0,0 +1,31 @@
+//
+//
+//  ForecastContainerViewController.swift
+//
+//  Created by Tomasz Idzi on 03/03/2021
+//  Copyright © 2021 Tomasz Idzi s.r.l. All rights reserved.
+//
+
+
+import UIKit
+
+class ForecastContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
