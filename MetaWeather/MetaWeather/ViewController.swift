dcf3380a2c5701d7f507042a394c29613a402b84
Tomasz Idzi <idzitomek@gmail.com>
2021-03-03 21:36:12 +0100

    Create new project.

---
+++ MetaWeather/MetaWeather/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+//  ViewController.swift
+//
+//  Created on 03/03/2021
+//  Copyright © 2021 IQUII s.r.l. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
