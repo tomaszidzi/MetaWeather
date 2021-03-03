d50b3e335c37ba06de4ac9f623997619d0ede62c
Tomasz Idzi <idzitomek@gmail.com>
2021-03-03 22:14:54 +0100

    Changed file header.

--- MetaWeather/MetaWeather/ForecastContainer/ForecastContainerViewController.swift
+++ MetaWeather/MetaWeather/ForecastContainer/ForecastContainerViewController.swift
@@ -3,7 +3,7 @@
 //  ForecastContainerViewController.swift
 //
 //  Created by Tomasz Idzi on 03/03/2021
-//  Copyright © 2021 Tomasz Idzi s.r.l. All rights reserved.
+//  Copyright © 2021 Tomasz Idzi. All rights reserved.
 //
 
 
