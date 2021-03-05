3d5d9f6e45b29ad67078800d2cc2e4f317b2dce1
Tomasz Idzi <idzitomek@gmail.com>
2021-03-05 17:23:51 +0100

    Added tests to project.

---
+++ MetaWeather/MetaWeatherTests/MetaWeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//
+//  MetaWeatherTests.swift
+//
+//  Created by Tomasz Idzi on 05/03/2021
+//  Copyright © 2021 Tomasz Idzi. All rights reserved.
+//
+
+
+import XCTest
+
+class MetaWeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
